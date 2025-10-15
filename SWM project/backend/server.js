4eddb9d2283f4b7ca8c50b70257d0df858d97faa
nisalG <nimgunathilaka021@gmail.com>
2025-10-16 02:53:35 +0530

    Add environment configuration and MongoDB connection setup
    - Created .env file for storing MongoDB connection string
    - Added .gitignore to exclude node_modules
    - Updated server.js to connect to MongoDB and listen on port 4000 ✅✅

--- SWM project/backend/server.js
+++ SWM project/backend/server.js
@@ -1,18 +1,28 @@
 // server.js
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+
 
 // ✅ Middleware
 app.use(cors());
 app.use(express.json());
 
-// ✅ Start server
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+
+let mongoURL = process.env.MONGO_URL;
+
+mongoose.connect(mongoURL);
+let connection = mongoose.connection;
+connection.once("open", ()=>{
+  console.log("MongoDB connection established successfully!")
+})
+
+
+app.listen(4000, ()=>{
+  console.log('listening on Port 4000')
 });
